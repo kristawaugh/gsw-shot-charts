95edc1a17638bcb23759d087fd6e51c3f5f13f85
Krista Waugh <kristawaugh@berkeley.edu>
2019-03-09 17:27:16 -0800

    column names and shot yes and no

--- code/make-shots-data.R
+++ code/make-shots-data.R
@@ -5,36 +5,40 @@ output: github_document
 library(dplyr) 
 library(tidyverse)
 library(caret)
+library(magrittr)
 setwd("/users/kristawaugh/Documents/Spring19/Stat133/workout01/code")
   
-iguodala0 <- read.csv("../data/andre-iguodala.csv", stringsAsFactors = FALSE, sep = ",")
-iguodala0
-
-iguodala <- dplyr::mutate(iguodala0, Name = "Andre Iguodala")
+iguodala00 <- read.csv("../data/andre-iguodala.csv", stringsAsFactors = FALSE, sep = ",")
+iguodala0 <- iguodala00 %>% mutate(shot_made_flag = recode(shot_made_flag, "y" = "shot_yes", "n" = "shot_no"))
+iguodala <- mutate(iguodala0, Name = "Andre Iguodala")
 iguodala
 
-curry0 <- read.csv("../data/stephen-curry.csv", stringsAsFactors = FALSE, sep = ",")
-curry0
 
-curry <- dplyr::mutate(curry0, Name = "Stephen Curry")
+curry00 <- read.csv("../data/stephen-curry.csv", stringsAsFactors = FALSE, sep = ",")
+curry0 <- curry00 %>% mutate(shot_made_flag = recode(shot_made_flag, "y" = "shot_yes", "n" = "shot_no"))
+curry <- mutate(curry0, Name = "Stephen Curry")
 curry
 
-green0 <- read.csv("../data/draymond-green.csv", stringsAsFactors = FALSE, sep = ",")
-green0
 
-green <- dplyr::mutate(green0, Name = "Draymond Green")
+
+green00 <- read.csv("../data/draymond-green.csv", stringsAsFactors = FALSE, sep = ",")
+green0 <- green00 %>% mutate(shot_made_flag = recode(shot_made_flag, "y" = "shot_yes", "n" = "shot_no"))
+green <- mutate(green0, Name = "Draymond Green")
 green
 
-durant0 <- read.csv("../data/kevin-durant.csv", stringsAsFactors = FALSE, sep = ",")
-durant0
 
-durant <- dplyr::mutate(durant0, Name = "Kevin Durant")
+durant00 <- read.csv("../data/kevin-durant.csv", stringsAsFactors = FALSE, sep = ",")
+durant0 <- green00 %>% mutate(shot_made_flag = recode(shot_made_flag, "y" = "shot_yes", "n" = "shot_no"))
+durant <- mutate(durant0, Name = "Kevin Durant")
 durant
 
-thompson0 <- read.csv("../data/klay-thompson.csv", stringsAsFactors = FALSE, sep = ",")
-thompson0
 
-thompson <- dplyr::mutate(thompson0, Name = "Klay Thompson")
+thompson00 <- read.csv("../data/klay-thompson.csv", stringsAsFactors = FALSE, sep = ",")
+thompson0 <- thompson00 %>% mutate(shot_made_flag = recode(shot_made_flag, "y" = "shot_yes", "n" = "shot_no"))
+thompson <- mutate(thompson0, Name = "Klay Thompson")
 thompson
+
+
+
 
 
